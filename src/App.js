c7032ea65ae0e44b7daecf424d39b4c669394d05
BGA admin <digital@bettergov.org>
2023-04-18 14:28:47 -0500

    Push MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Masonry from 'react-masonry-css';
+import img from './images/1.jpeg'
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  
+  useEffect(() => {
+    const importImages = async () => {
+      const images = [];
+      for (let i = 1; i <= 80; i++) {
+        const image = await import(`./images/${i}.jpeg`);
+        images.push(image.default);
+      }
+      setImages(images);
+    };
+    importImages();
+  }, []);
+
+
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  <Masonry
+  breakpointCols={8}
+  className="my-masonry-grid"
+  columnClassName="my-masonry-grid_column">
+  {/* array of JSX items */}
+  {images.map((image, index) => (
+          <div><a data-lightbox="gallery" href={image} ><img key={index} src={image} alt={`Image ${index + 1}`} /></a></div>
+        ))}
+</Masonry>
+
     </div>
   );
 }
